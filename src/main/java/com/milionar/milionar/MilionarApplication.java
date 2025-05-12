3edfa92f8f05d8b6ac62d666d27dd242df17eecd
Patrik Demjan <patrikdemjan@MacBook-Air-uzivatela-Patrik.local>
2025-05-12 22:33:15 +0200

    Initial commit - Millionaire game

---
+++ src/main/java/com/milionar/milionar/MilionarApplication.java
@@ -0,0 +1,13 @@
+package com.milionar.milionar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilionarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilionarApplication.class, args);
+	}
+
+}
